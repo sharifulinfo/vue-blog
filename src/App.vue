1d139f5be38781c24548bed707483d024827994c
Shariful Islam <shariful.info55@gmail.com>
2020-07-15 22:38:30 +0600

    slider support

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="app">
     <nab/>
-    <home/>
+    <div class="container">
+      <home/> 
+      <sliders>
+          <slide></slide>
+      </sliders>
+    </div>
   </div>
 </template>
 
@@ -9,11 +14,12 @@
 // import HelloWorld from './components/HelloWorld.vue'
 import nab from './components/nav.vue'
 import home from './components/home.vue'
+import sliders from './components/sliders.vue'
 
 export default {
   name: 'App',
   components: {
-    nab,home
+    nab,home,sliders
   }
 }
 </script>
@@ -21,10 +27,5 @@ export default {
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
